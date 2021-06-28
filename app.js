73a93ce0a9a469eef75881b52764504931f6f6f9
Casey Geist <deepimpact5@yahoo.com>
2021-06-28 16:45:01 -0400

    refactored script

--- app.js
+++ app.js
@@ -1,48 +1,64 @@
 // pseudo-code
-// goal: clicks a button AND generates a new div with new color
-
-// 1. get the the button
+// goal: clicks a button AND generates a new div with new color and text
+alert("starting script");
+	// 1. get the the button
 const btnGen = document.querySelector('#btn-generate');
 
-// 2. be able to click the button
-// 3. After button click, generate div
+	// 2. be able to click the button
+	// 3. After button click, generate div
+
 btnGen.addEventListener('click', function () {
+	
+	alert("inside event listener function");
+	
+	const userTailwindColor;
+	const userContent;
+	let success;
+		
+    	// 1. Prompt for user input
 
-    // collectInput()
-    // checkInput()
-    // createDiv()
+    collectInput(userTailwindColor, userContent);   
+    
+    	// 2. Check that the input is valid
 
-    collectInput(); //string
+	success = checkInput(userTailwindColor, userContent);
+	
+    if( !success ) {	// 3a. if input not valid, alert and stop
+    	alert("Input not valid. Please try again.");
+    	return;
 
-    //@TODO: Refactor this block to simplify this EventListner
-    const userTailwindColor = window.prompt('What TW Color?'); //string
-    const userContent = window.prompt('What Text?'); // string
-
-    // 1. check that the input is valid
-    // 2. if it's not valid, give feedback
-
-    checkInput(); //true
-    // 3. if not OK stop
-    // return ;
-
-    // 4. if OK
+    	// 3b. Otherwise if OK
     createNewDiv(userTailwindColor, userContent);
 
 });
 
-function collectInput(){
-    //@TODO: collect user input
+function collectInput(color, text) {
+
+    color = window.prompt('What TW Color?', 'blue-300'); //string
+    text = window.prompt('What Text?', 'a new container'); // string
 }
 
-function checkInput() {
-    // @TODO: check if input is valid
-    return true;
+function checkInput(color, text) {
+    
+    if( isTailwindColor(color)  &&  typeof(text) == string)
+    	return true;
+    	
+    return false;
+}
+
+function isTailwindColor(color) {
+	
+	if( typeof(color) != string )
+		return false;
+		
+	// @TODO ensure string for color is in exact Tailwind format
+	return true;
 }
 
 // [D]ry [R]epeat [Y]ourself
 function createNewDiv(tailwindBgColor, content) {
     const div = document.createElement('div');
-    div.innerHTML = `<h3 class="text-9xl ${tailwindBgColor} p-3">${content}</h3>`; // string interpolation
+    div.innerHTML = `<h3 class="text-9xl bg-${userTailwindColor} p-3">${userContent}</h3>`; // string interpolation
 
     document.body.appendChild(div);
 
