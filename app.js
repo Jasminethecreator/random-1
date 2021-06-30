a8d19dec64fe073510cc0dfb51d3c73226aa42f9
Dele O <dele@authoritywit.com>
2021-06-30 12:32:53 -0400

    rfc: fleshing out a color generator Read all the comments, solve the @TODO on line 96

--- app.js
+++ app.js
@@ -1,57 +1,106 @@
-// pseudo-code
-// goal: clicks a button AND generates a new div with new color
+// Our goal: A user clicks a button AND generates a new div with new color
 
-// 1. get the the button
-const btnGen = document.querySelector('#btn-generate');
+// When approaching this, it's often a great idea to start with a pseudo-code
+// something like:
+// 1. Get the button from the the DOM
+// 2. After getting the button, when a user clicks...
+// 3. Generate a div
 
-// 2. be able to click the button
-// 3. After button click, generate div
-btnGen.addEventListener('click', function () {
+// with this set, we go to some solvng
 
-    // collectInput()
-    // checkInput()
-    // createDiv()
+// 1. Get the button from the the DOM
+const btnGen = document.querySelector("#btn-generate");
 
-    collectInput(); //string
+// 2. After getting the button, when a user clicks...
+btnGen.addEventListener("click", function () {
+  // this involves a few steps, so let's do a first pass
+  // we're using the "prompt" to get the user input
+  // See: http://developer.mozilla.org/en-US/docs/Web/API/Window/prompt
+  // (for more fun reading, see the "See also" in the link above)
+  const userTailwindColor = window.prompt("What TW Color?"); //string
+  const userContent = window.prompt("What Text?"); // string
 
-    //@TODO: Refactor this block to simplify this EventListner
-    const userTailwindColor = window.prompt('What TW Color?'); //string
-    const userContent = window.prompt('What Text?'); // string
+  // collectInput()
+  // checkInput()
+  // createDiv()
 
-    // 1. check that the input is valid
-    // 2. if it's not valid, give feedback
+  // 3. Generate a div
+  // `createDiv` is a function we created off a refactor
+  createNewDiv(userTailwindColor, userContent);
 
-    checkInput(); //true
-    // 3. if not OK stop
-    // return ;
+  // While the above works, we can do better
+  // L23 and L24 are repeating themselves, it seems a good candidate for a refactor
+  // let's think out loud:
 
-    // 4. if OK
-    createNewDiv(userTailwindColor, userContent);
+  // 1. we collecting inputs
+  // 2. we saving those input as a variables
+  // 3. We're acting on those
 
+  // OK, time for a refactor:
+  // 1. Collect input
+  // function 1:
+  //  collectInput()
+  // since we're collecting user input,
+  // we want to be sure those input are right, so... we add another function
+  // checkInput()
+
+  // function 2:
+  // 2. Create divs
+  // createNewDiv()
+
+  // with all these functions defined, we can simply our code as:
+  //  const userTailwindColor =  collectInput('What TW Color?'); //string
+  //  const userContent =  collectInput('What Text?'); // string
+  // createNewDiv()
+
+  // and since we don't really want to trust our users as they can write anything
+  // we want to validate things with their input, so we can do:
+
+  // const isTWClassValid = checkInput(userTailwindColor) // stores the bool value of this check
+  // const isContentValid = checkInput(userContent)
+
+  // this will both return booleans, and as part of our problem statement..
+  // we want to ONLY create a div IF the input is correct (that is, it must be a string)
+  // so we can write the code like this:
+
+  // if(TWCheck && userCheck) createNewDiv()
+
+  // the full code will look like this (version 3):
+
+  /**
+   * const userTailwindColor =  collectInput('What TW Color?');
+   * const userContent =  collectInput('What Text?');
+   *
+   * const isTWClassValid = checkInput(userTailwindColor);
+   * const isContentValid = checkInput(userContent);
+   *
+   * if(TWCheck && userCheck) createNewDiv();
+   */
+
+  // well, that's nice but we're setting variables over and over, we can clean this up a bit
+  // with some eye towards Functional Programming
+
+  // an alternative take can look like this (version 4):
+
+  /**
+   * const isTWClassValid = checkInput(collectInput('What TW Color?'));
+   * const isContentValid = checkInput(collectInput('What Text?'));
+   *
+   * if(isContentValid && isTWClassValid) createDiv();
+   */
+
+  // I think you will agree that the version 4 is simpler, readable and easier to reason about
+  // ALSO... take special note on how I name my variables — I give top priority to clarity.
 });
 
-function collectInput(){
-    //@TODO: collect user input
-}
-
-function checkInput() {
-    // @TODO: check if input is valid
-    return true;
-}
+//@TODO: Declare all the other functions to make the above work
 
 // [D]ry [R]epeat [Y]ourself
 function createNewDiv(tailwindBgColor, content) {
-    const div = document.createElement('div');
-    div.innerHTML = `<h3 class="text-9xl ${tailwindBgColor} p-3">${content}</h3>`; // string interpolation
+  const div = document.createElement("div");
+  div.innerHTML = `<h3 class="text-9xl ${tailwindBgColor} p-3">${content}</h3>`; // string interpolation
 
-    document.body.appendChild(div);
+  document.body.appendChild(div);
 
-    window.alert('Your div was created');
+  window.alert("Your div was created");
 }
-
-
-
-
-
-
-
